81b459beadc1dc37f784993de2211d321ed2f8f9
gkedi <gedeonkedi4@gmail.com>
2026-04-01 20:47:54 +0100

    feat: add timer state structure

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-'use strict';
+"use strict";
 
-console.log('App initialized');
+const timer = {
+  duration: 0,
+  startTime: null,
+  remaining: 0,
+  isRunning: false,
+};
+
+console.log(timer);
